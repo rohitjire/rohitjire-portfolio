19fc0d70d96e676d928f0b37a19c487e420e19ce
ROHIT JIRE <rohit.jire@t-systems.com>
2022-11-07 17:16:15 +0530

    hero component added

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -22,7 +22,7 @@ function BackgroundCircle({}: Props) {
       <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className=" rounded-full border border-[#333333] h-[300px] w-[300px] absolute mt-52" />
       <div className=" rounded-full border border-[#333333] h-[500px] w-[500px] absolute mt-52" />
-      <div className=" rounded-full border border-[#F7AB0A] h-[650px] w-[650px] opacity-10 absolute mt-52 animate-pulse" />
+      <div className=" rounded-full border border-[#7d632a] h-[650px] w-[650px] opacity-10 absolute mt-52 animate-pulse" />
       <div className=" rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52 animate-pulse" />
     </motion.div>
   );
