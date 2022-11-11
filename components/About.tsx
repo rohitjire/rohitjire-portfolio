8185bd34e9808649d57b35f2bb818bc4fca7ec6d
ROHIT JIRE <rohit.jire@t-systems.com>
2022-11-11 23:41:17 +0530

    added about section

--- components/About.tsx
+++ components/About.tsx
@@ -5,8 +5,17 @@ type Props = {};
 
 function About({}: Props) {
   return (
-    <div
-      className="h-screen flex flex-col relative text-center md: text-left md:flex-row max-w-7xl
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl
      px-10 justify-evenly mx-auto items-center"
     >
       <h3
@@ -18,17 +27,39 @@ function About({}: Props) {
       <motion.img
         initial={{
           x: -200,
+          opacity: 0,
         }}
         transition={{
           duration: 1.2,
         }}
         whileInView={{
           x: 0,
+          opacity: 1,
         }}
         viewport={{ once: true }}
         src="https://picsum.photos/id/237/500/500"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+        md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
       />
-    </div>
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-slate-200">Little</span>{" "}
+          Background
+        </h4>
+        <p className="text-base">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum
+        </p>
+      </div>
+    </motion.div>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,9 @@ export default function Home() {
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experiecne */}
 
+      {/* Experiecne */}
+      
       {/* Skills */}
 
       {/* Projects */}
