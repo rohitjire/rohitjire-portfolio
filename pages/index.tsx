6677066ac7b52d016c8b7507f684afee61122a8d
ROHIT JIRE <rohit.jire@t-systems.com>
2022-12-26 16:49:41 +0530

    added footer icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import { urlFor } from "../sanity";
 import { Experience, PageInfo, Project, Skill, Social } from "../typings";
 import { fetchExperiences } from "../utils/fetchExperieces";
 import { fetchPageInfo } from "../utils/fetchPageInfo";
@@ -78,8 +79,7 @@ export default function Home({
             <img
               className="h-10 w-10 rounded-full filter grayscale 
               hover::grayscale-0 cursor-pointer"
-              src=""
-              alt=""
+              src={urlFor(pageInfo.profilePic).url()}
             />
           </div>
         </footer>
