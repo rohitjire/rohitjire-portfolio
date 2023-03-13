823f2f0f95779d09fba3bd3922d2b9427a3ee9d2
Rohit Jire <rohitjire55@gmail.com>
2023-03-13 12:55:44 +0530

    removed props

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,13 @@ import { fetchProjects } from "../utils/fetchProjects";
 import { fetchSkills } from "../utils/fetchSkills";
 import { fetchSocials } from "../utils/fetchSocials";
 
-type Props = {
-  pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
-  socials: Social[];
-};
+// type Props = {
+//   pageInfo: PageInfo;
+//   experiences: Experience[];
+//   skills: Skill[];
+//   projects: Project[];
+//   socials: Social[];
+// };
 
 export default function Home() {
   return (
@@ -36,24 +36,3 @@ export default function Home() {
   );
 }
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = await fetchExperiences();
-  const skills: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
-  const socials: Social[] = await fetchSocials();
-
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
-    },
-    // Next.js will attempt t re-generate the page
-    // When a request comes in
-    // At most once every 10 seconds
-    revalidate: 10,
-  };
-};
