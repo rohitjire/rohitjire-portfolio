249b5b40a914c87e7f71a96b421f43fbbd1f5564
Rohit Jire <rohitjire55@gmail.com>
2023-03-13 12:51:50 +0530

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,13 +24,7 @@ type Props = {
   socials: Social[];
 };
 
-export default function Home({
-  pageInfo,
-  experiences,
-  projects,
-  skills,
-  socials,
-}: Props) {
+export default function Home() {
   return (
     <div
       className="bg-[rgb(36,36,36)] text-white h-screen snap-y 
